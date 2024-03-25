d7b8dde635af5b45a709102a928e827f9106dc36
soumeyahaloui <soumeyahaloui@gmail.com>
2024-03-25 02:03:39 +0000

    updated

--- flask_server.py
+++ flask_server.py
@@ -35,9 +35,9 @@ def create_user_table():
         # Connect to your MySQL database
         connection = pymysql.connect(
             host='sql11.freesqldatabase.com',
-            user='sql11681109',
-            password='C1ex6u3Uaa',
-            db='sql11681109',
+            user='sql11694019',
+            password='wIJB3Bvi5t',
+            db='sql11694019',
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
